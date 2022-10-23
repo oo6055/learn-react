9935f653711c612e6621cbf839017b7ba4c4ccab
Ori Ofek <oriofek106@gmail.com>
2022-10-24 01:57:08 +0300

    try heroku

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -10,7 +10,7 @@ const CheckoutItem = ({cartItem, clearItem, removeItem, addItem}) =>
             <img alt='item' src={imageUrl} />
         </div>
         <span className="name" >{name}</span>
-        <span className="quantity">
+        <span className="quantity"> 
             <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
             <span className="value">{quantity}</span>
             <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
